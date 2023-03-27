664842d4b87c8af7770801b3c2dc68f0639af733
emma <emmasimp@users.noreply.github.com>
2023-03-27 16:05:23 +0100

    PEP8 fixes

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -16,14 +16,17 @@ def main(args):
     InFiles = args.infiles
     if not isinstance(InFiles, list):
         InFiles = [args.infiles]
-    
-    
+
     for filename in InFiles:
         measurement_data = models.read_variable_from_csv(filename)
-        
-        view_data = {'daily sum': models.daily_total(measurement_data), 'daily average': models.daily_mean(measurement_data), 'daily max': models.daily_max(measurement_data), 'daily min': models.daily_min(measurement_data)}
-        
+
+        view_data = {'daily sum': models.daily_total(measurement_data),
+                     'daily average': models.daily_mean(measurement_data),
+                     'daily max': models.daily_max(measurement_data),
+                     'daily min': models.daily_min(measurement_data)}
+
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
